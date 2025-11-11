202492809cb47ceceb2fb09eaa107965d1edd827
Syed-Hammad <hammadshah7218@gmail.com>
2025-11-11 22:21:32 +0500

    add lightrays on background

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import Navigation from "@/components/navigation"
+import LightRays from "@/components/lightrays"
 
 const geistSans = Geist({
   subsets: ["latin"],
@@ -29,6 +30,21 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+
+  <div style={{ width: '100%', height: '600px', position: 'fixed', top: 0, left: 0, zIndex: -1 }}>
+  <LightRays
+    raysOrigin="top-center"
+    raysColor="#498ac6ff"
+    raysSpeed={1.3}
+    lightSpread={3.0}
+    rayLength={2.0}
+    followMouse={true}
+    mouseInfluence={0.1}
+    noiseAmount={0.1}
+    distortion={0.05}
+    className="custom-rays"
+  />
+</div>
         <Navigation />
         <main className="pt-16">{children}</main>
       </body>
